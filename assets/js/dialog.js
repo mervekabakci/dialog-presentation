f56626622d6e1e322289296cb311109ac881eab7
TriosMerve <merve@trios.com.tr>
2025-06-14 20:08:39 +0300

    kullanım sekilleri ve ıcerık guncellemesı yapıldı

--- assets/js/dialog.js
+++ assets/js/dialog.js
@@ -12,3 +12,37 @@ function showBasicDialog(e){
 //dialog ac kapat tetikleyici butonlar
 openModal.addEventListener("click", showBasicDialog);
 closeModal.addEventListener("click", closeBasicDialog);
+
+
+//Çoklu kullanım
+const dialogs = document.querySelectorAll(".dialog");
+const dialogButtons = document.querySelectorAll('.openDialogButton');
+const closeDialogButtons = document.querySelectorAll('.closeDialog');
+const body = document.querySelector("body");
+const createDiv = document.createElement('div');
+
+function handleClick(e){
+    e.preventDefault();
+    const nextDialog = this.nextElementSibling;
+    nextDialog.show();
+
+    body.style.overflow = "hidden";
+     
+    body.append(createDiv);
+    createDiv.classList.add("dialogOverlay");
+}
+
+for(const dialogButton of dialogButtons){
+    dialogButton.addEventListener("click", handleClick);
+}
+
+function handleClickClose(e){
+    e.preventDefault();
+    this.parentElement.close();
+    createDiv.remove();
+    body.style.overflow = "auto";
+}
+
+for(const closeDialog of closeDialogButtons){
+    closeDialog.addEventListener("click", handleClickClose);
+}
